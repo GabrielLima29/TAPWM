b24bd8ec102772770f0b3145678b855c064ab161
GabrielLima29 <101425770+GabrielLima29@users.noreply.github.com>
2023-11-01 17:55:40 -0300

    Add files via upload

---
+++ Atividade 13/index.js
@@ -0,0 +1,13 @@
+//1) metodo construtor
+function Retangulo(x, y){
+    this.x = x;
+    this.y = y;
+    var resultado;
+
+    this.Calcular = function() {
+        resultado = this.x * this.y;
+        return resultado;
+    }
+}
+    varcalculo = new Retangulo("2", "3");
+    alert(aluno4.MostraDados());
